96266aa2264e589088a9a71f471bfde3bbf12676
German Robayo Paz <germanrobayo33@gmail.com>
2017-08-14 18:38:09 -0400

    Standarized

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // entry: es la que decide que se va a colocar en el dist. en app.js por ejemplo se decidira que se va a usar
@@ -7,31 +7,30 @@ module.exports = {
   entry: './src/app.js',
   // el output en un solo archivo
   output: {
-    filename: 'bundle.js', 
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   // module.rules contiene las reglas o mandatos que ejecutara webpack cada vez que lo ejecutemos
   module: {
     rules: [
-      
       {
         test: /\.scss$/,
         use: [{
-            loader: "style-loader" // creates style nodes from JS strings
+          loader: 'style-loader' // creates style nodes from JS strings
         }, {
-            loader: "css-loader" // translates CSS into CommonJS
+          loader: 'css-loader' // translates CSS into CommonJS
         }, {
-            loader: "sass-loader" // compiles Sass to CSS
+          loader: 'sass-loader' // compiles Sass to CSS
         }]
       },
       {
         test: /\.pug$/,
-        use: ['html-loader','pug-html-loader']
+        use: ['html-loader', 'pug-html-loader']
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: 'babel-loader'
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
@@ -43,7 +42,7 @@ module.exports = {
   },
   devServer: {
     // The following 2 lines enable to check it via IP
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     disableHostCheck: true,
     contentBase: './dist'
   },
